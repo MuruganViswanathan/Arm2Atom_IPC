3cd4a68b7a6c581e05334fa94d16b7303fcad9ba
Murugan Viswanathan <murugan.viswanathan2@cognizant.com>
2017-09-08 18:24:06 -0700

    Build with cmake

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Arm2Atom_IPC
+
+# Build instructions:
+
+#for socket server and client
+cd Socket
+mkdir build
+cd build
+cmake ..
+make
+# the above will generate sockserver and sockclient
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/murugan/code/Arm2Atom_IPC
+# Build directory: /home/murugan/code/Arm2Atom_IPC/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(Socket)
