45d4eea84b2182513fe33d4887493d7e37f39353
Murugan Viswanathan <murugan.viswanathan2@cognizant.com>
2017-09-08 18:29:17 -0700

    Build with cmake

--- build/Socket/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/murugan/code/Arm2Atom_IPC/Socket
-# Build directory: /home/murugan/code/Arm2Atom_IPC/build/Socket
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
